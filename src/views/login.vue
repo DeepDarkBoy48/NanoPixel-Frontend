5b6a29ed360a10d3b3f69f25caf9a950ed97a3d2
DeepDarkBoy48 <warghost200818@gmail.com>
2025-07-30 11:08:41 +0800

    fix: 修复 checkRePassword 函数变量提升问题
    - 将 checkRePassword 函数定义移到 rules 对象之前
    - 清理 registerData 中的无用属性
    - 解决 'Cannot access checkRePassword before initialization' 错误

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,7 +3,43 @@ import { User, Lock } from '@element-plus/icons-vue'
 import { ref } from 'vue'
 //控制注册与登录表单的显示， 默认显示注册
 const isRegister = ref(false)
+
+//定义数据模型对象
+const registerData = ref({
+    username: '',
+    password: '',
+    rePassword: ''
+})
+
+//校验密码的函数
+const checkRePassword = (rule, value, callback) => {
+    if (value === '') {
+        callback(new Error('请再次确认密码'))
+    } else if (value !== registerData.value.password) {
+        callback(new Error('请确保两次输入的密码一样'))
+    } else {
+        callback()
+    }
+}
+
+//定义表单校验规则
+const rules = {
+    username: [
+        { required: true, message: '请输入用户名', trigger: 'blur' },
+        { min: 5, max: 16, message: '长度为5~16位非空字符', trigger: 'blur' }
+    ],
+    password: [
+        { required: true, message: '请输入密码', trigger: 'blur' },
+        { min: 5, max: 16, message: '长度为5~16位非空字符', trigger: 'blur' }
+    ],
+    //自定义规则
+    rePassword: [
+        { validator: checkRePassword, trigger: 'blur' }
+    ]
+}
+
 </script>
+
 
 <template>
     <el-row class="login-page">
@@ -12,18 +48,20 @@ const isRegister = ref(false)
             <!-- 注册表单 -->
             <!-- v-if:是否显示注册或者登陆 -->
              
-            <el-form ref="form" size="large" autocomplete="off" v-if="isRegister">
+            <el-form ref="form" size="large" autocomplete="off" v-if="isRegister" v-model="registerData" :rules="rules">
                 <el-form-item>
                     <h1>注册</h1>
                 </el-form-item>
-                <el-form-item>
-                    <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+                <el-form-item prop="username">
+                    <el-input :prefix-icon="User" placeholder="请输入用户名" v-model="registerData.username"></el-input>
                 </el-form-item>
-                <el-form-item>
-                    <el-input :prefix-icon="Lock" type="password" placeholder="请输入密码"></el-input>
+                <el-form-item prop="password">
+                    <el-input :prefix-icon="Lock" type="password" placeholder="请输入密码"
+                        v-model="registerData.password"></el-input>
                 </el-form-item>
-                <el-form-item>
-                    <el-input :prefix-icon="Lock" type="password" placeholder="请输入再次密码"></el-input>
+                <el-form-item prop="rePassword">
+                    <el-input :prefix-icon="Lock" type="password" placeholder="请输入再次密码"
+                        v-model="registerData.rePassword"></el-input>
                 </el-form-item>
                 <!-- 注册按钮 -->
                 <el-form-item>
@@ -38,15 +76,15 @@ const isRegister = ref(false)
                 </el-form-item>
             </el-form>
             <!-- 登录表单 -->
-            <el-form ref="form" size="large" autocomplete="off" v-else>
+            <el-form ref="form" size="large" autocomplete="off" v-else :rules="rules" v-model="registerData">
                 <el-form-item>
                     <h1>登录</h1>
                 </el-form-item>
-                <el-form-item>
-                    <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+                <el-form-item prop="username">
+                    <el-input :prefix-icon="User" placeholder="请输入用户名" v-model="registerData.username"></el-input>
                 </el-form-item>
-                <el-form-item>
-                    <el-input name="password" :prefix-icon="Lock" type="password" placeholder="请输入密码"></el-input>
+                <el-form-item prop="password">
+                    <el-input name="password" :prefix-icon="Lock" type="password" placeholder="请输入密码" v-model="registerData.password"></el-input>
                 </el-form-item>
                 <el-form-item class="flex">
                     <div class="flex">
