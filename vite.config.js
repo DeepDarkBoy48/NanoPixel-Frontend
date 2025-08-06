350a6f9d7a5ec9417c13705d3a94ccecd8a91b96
DeepDarkBoy48 <warghost200818@gmail.com>
2025-08-06 18:15:27 +0800

    配置

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         //获取路径中包含了/api的请求
-        // target: "http://localhost:8081", //后台服务所在的源
-        target: "http://x.smashteches.com:8081", //后台服务所在的源
+        target: "http://localhost:8081", //后台服务所在的源
+        // target: "http://x.smashteches.com:8081", //后台服务所在的源
         changeOrigin: true, //修改源
         rewrite: (path) => path.replace(/^\/api/, ""), ///api替换为''
       },
